81a87bcb42f63cb511c41af3fee0dd63a2142955
Leo Feng <thcheetah777@gmail.com>
2023-12-01 22:05:09 +0200

    🎯 target counter

--- level_manager.gd
+++ level_manager.gd
@@ -2,12 +2,21 @@ extends Node
 class_name LevelManager
 
 @export var targets: Array[Target]
+@export var target_counter: HBoxContainer
+@export var target_ui_lit: Texture2D
+@export var target_ui_unlit: Texture2D
 
 var targets_left: Array[Target]
 
 func _enter_tree() -> void:
 	Globals.level_manager = self
-	targets_left = targets
+	targets_left = targets.duplicate()
+
+func _ready() -> void:
+	for target in targets:
+		var target_ui = TextureRect.new()
+		target_ui.texture = target_ui_unlit
+		target_counter.add_child(target_ui)
 
 func update_current_target():
 	targets_left.front().on_opened()
@@ -15,6 +24,8 @@ func update_current_target():
 func pop_target():
 	var popped_target = targets_left.pop_front()
 	popped_target.on_touched()
+	target_counter.get_child(targets.size() - targets_left.size() - 1).texture = target_ui_lit
+
 	if targets_left.size() <= 0:
 		get_tree().reload_current_scene()
 	else:
